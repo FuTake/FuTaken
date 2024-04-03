32b8c9842e6fa5f04e698bc974ddc792f7004b24
zhaohaoguang <zhaohaoguang@bonc.com.cn>
2024-04-03 11:12:22 +0800

    理解useRef-useContext

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Profile from './Gallery';
 import { Gallery2 } from './Gallery';
 import { Clock, UseTime } from './Clock.js';
 // 任何use开头的函数都是hook,这是个特殊函数
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext, createContext, useReducer, useRef } from "react";
 // immer代理解决多层次对象属性更新的问题
 import { useImmer } from 'use-immer';
 
@@ -18,10 +18,10 @@ const user = {
   imageSize: 90,
 };
 
-const products = [
-  {title: 'zhangsan', id:1},
-  {title: 'lisi', id:2},
-  {title: 'wangwu', id:3},
+const person = [
+  {userName: 'zhangsan', id:1},
+  {userName: 'lisi', id:2},
+  {userName: 'wangwu', id:3},
 ];
 
 // 永远不要在组件中嵌套组件
@@ -96,7 +96,7 @@ export function Bio() {
   );
 }
 
-// 将Bio中的html做为children引入IncludeBio的组件中
+// 将Bio中的html做为children引入IncludeBio的组件中。children在react中固定认为是<>子标签</>，这里子标签的内容
 export function IncludeBio({children}){
   // 写html需要用到();
   return (
@@ -319,4 +319,202 @@ export function InputUpdate(){
   );
 }
 
+// 放在外面就可以生效
+let nextId = 0;
+export function UpdateArray(){
+  const [userName, setUserName] = useState("");
+  // const [userList, setUserList] = useState(...person);
+  const [userList, setUserList] = useState([]);
+  // 放在里面 nextId++就不会生效
+  // let nextId = 0;
 
+  const colors = ["yellow", "black", "blue", "red", "gray", "green", "purple"];
+
+  // 修改列表中指定id的被选状态
+  function onToggle(id, seen){
+    // 浅拷贝的方式
+      const tempList = [...userList];
+      const tempUser = tempList.find(
+        a => a.id === id
+      );
+      tempUser.seen = seen;
+      setUserList(tempList);
+  }
+
+  function onToggleDeep(id, seen){
+    // 深拷贝的方式
+    setUserList(userList.map(tempUser => {
+      if(id === tempUser.id){
+        // 这个对象有变动，就返回 创建的新对象。这个时候数组里就会包含原来的老对象和现在创建的新对象
+        return {...tempUser, seen: seen};
+      }else{
+        return tempUser;
+      }
+    }))
+  }
+
+  return (
+    <>
+      <div><input value={userName} onChange={e => setUserName(e.target.value)} /></div>
+      <div>{nextId}</div>
+      <div>
+        <button onClick={() => {
+          setUserList([
+            ...userList,
+            {id:nextId++, userName: userName, seen: nextId%2 === 1}
+          ]);
+          console.log(userList);
+        }}>提交</button>
+      </div>
+      
+      <div>
+        <button onClick={()=>{
+          setUserList([
+            // 使用slice函数必须带上三个点，这里先截取数组下标0的元素。slice函数不会修改原数组的元素
+            ...userList.slice(0, 1),
+            // 补充元素做为数组下标1的值
+            {id: nextId++, userName: userName},
+            // 将数组剩下的元素补充到新数组
+            ...userList.slice(1)
+          ]);
+          console.log(userList);
+        }}>插入第二行</button>
+      </div>
+
+      <div>
+        <button onClick={()=>{
+          // reverse会直接修改userList的数据，所以不建议这样使用
+          // setUserList(userList.reverse());
+          // 这是一种浅拷贝，修改userList的数据会直接影响到tempList的数据，从而影响到setUserList入参的值
+          const tempList = [...userList];
+          userList[0].userName = '名字被修改';
+          tempList.reverse();
+          setUserList(tempList);
+        }}>
+          反转
+        </button>
+      </div>
+
+
+      <div>
+        <ul>
+          {userList.map(value => (
+            <>
+            {/*  颜色的修改不起作用 */}
+              <li key={value.id} id={value.id} style={{color:colors[Math.floor(Math.random * 7)]}}>
+                <input type="checkbox" checked={value.seen} onChange={e => {
+                  onToggleDeep(
+                    value.id,
+                    e.target.checked
+                  );
+                }} />
+                {value.userName}
+                <button key={value.id} onClick={() => {
+                  setUserList(
+                    userList.filter(a => a.id !== value.id)
+                  );
+                }}>删除</button>
+              </li>
+            </>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+
+//useReducer 功能不理解 https://react.docschina.org/learn/managing-state 提取状态逻辑到 reducer 中 ⚠
+
+export function Section({ level, children }){
+  return (
+    <section className='section'>
+      <LevelContext.Provider value={level}>
+        {/* 通过LevelContext.Provider将value传入Heading组件 */}
+        {children}
+      </LevelContext.Provider>
+    </section>
+  );
+}
+
+export const LevelContext = createContext(1);
+
+export function Heading({ level = null, children }){
+  // 使用useContext将level写在Section上，等价于 将level写在每个 子子标题 的用法
+  // 注释掉level = useCon...的语句，相当于不适用useContext实现 子子标题 的动态标签
+  // 释放level = useCon...语句，相当于使用useContext实现 除子子标题外 的动态标签
+  // if(level == null){
+  //   // useContext的调用不能依赖于某种条件，如这里的if
+    // level = useContext(LevelContext);
+  // }
+  switch(level) {
+    case 1:
+      return <h1>{children}</h1>
+    case 2:
+      return <h2>{children}</h2>
+    case 3:
+      return <h3>{children}</h3>
+    case 4:
+      return <h4>{children}</h4>
+    case 5:
+      return <h5>{children}</h5>
+    case 6:
+      return <h6>{children}</h6>
+    default:
+      return <h7>default {children}</h7>;
+  }
+}
+// 使用useContext让Heading使用最近section中配置的level
+export function ContextPractice(){
+  return (
+    <Section level={1}>
+      <Heading>主标题</Heading>
+      <Section level={2}>
+        <Heading>副标题</Heading>
+        <Heading>副标题</Heading>
+        <Heading>副标题</Heading>
+        <Section level={3}>
+          <Heading>子标题</Heading>
+          <Heading>子标题</Heading>
+          <Heading>子标题</Heading>
+          <Section>
+            <Heading level={4}>子子标题</Heading>
+            <Heading level={4}>子子标题</Heading>
+            <Heading level={4}>子子标题</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
+
+export default function RefTest(){
+  // 被useRef和useState修饰后的变量更像静态变量，不会因为渲染而重置为默认值
+  // ref.current会在改变后立刻生效 脱围机制
+  let ref = useRef(0);
+  // time改变后，会在下次渲染后生效
+  const [time, setTime] = useState(0);
+  //像是成员变量，每次创建相当于new RefTest，所以这里的temp每次渲染后默认值为0
+  let temp = 0;
+
+
+  function handleClick(){
+    ref.current = ref.current + 1;
+    // 这个+1会在下次渲染后生效，注释掉该行后，div.id=test标签将不会更新，并且alert和console.log中ref.current=temp，time恒为0
+    // 所以不触发渲染 temp就不会重置为默认值，这个过程更像成员变量
+    setTime(time + 1);
+    temp = temp + 1;
+    // console.log和alert显示的time要比ref.current小1，temp则只为1。这是渲染前的RefTest所以ref.current和temp的变更立即奏效，time的变更在下次渲染时奏效
+    // 并且使用ref不会让react重新渲染组件，所以ref更适合用于仅事件处理器需要的变量
+    alert("你点击了一次， ref.current:" + ref.current + " time:" + time + " temp:" + temp);
+    console.log(ref.current + " : " + time + " : " + temp);
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>点击</button>
+      {/* ref.current = time，temp恒为0。 先执行handleClick函数，然后重新渲染（重新渲染相当于new RefTest此时temp被重置，而time则显示为渲染时的数(即新RefTest的值)） */}
+      <div id="test">{ref.current + " " + time + " " + temp}</div>
+    </>
+  );
+}
